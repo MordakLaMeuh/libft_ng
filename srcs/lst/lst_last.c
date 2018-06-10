2e537bcb77baf6eb2ada1cc8ea2ad2a03a3820f3
vcombey <vcombey@student.42.fr>
2018-06-10 11:26:53 +0200

    merge bmickael an my libft

---
+++ srcs/lst/lst_last.c
@@ -0,0 +1,16 @@
+#include "list.h"
+
+void	*ft_lst_last(void *lst)
+{
+	t_gen_lst	*lst_cast;
+
+	lst_cast = lst;
+	while (lst_cast && lst_cast->next)
+		lst_cast = lst_cast->next;
+	return (lst_cast);
+}
+
+void	*ft_dlst_last(void *lst)
+{
+    return (ft_lst_last(lst));
+}
---
+++ srcs/lst/lst_rev.c
@@ -0,0 +1,21 @@
+#include "list.h"
+
+void	ft_lst_rev(void **lst)
+{
+	t_gen_lst *a;
+	t_gen_lst *b;
+	t_gen_lst *c;
+
+	a = NULL;
+	b = *lst;
+	c = b->next;
+	while (b)
+	{
+		b->next = a;
+		a = b;
+		b = c;
+		if (c)
+			c = c->next;
+	}
+	*lst = a;
+}
