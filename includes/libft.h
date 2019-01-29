46dd05b734de88df9daf4ad1f092a697a61d3b13
mordak <bmickael@student.42.fr>
2019-01-29 21:49:51 +0000

    reorganize header file

--- includes/libft.h
+++ includes/libft.h
@@ -10,14 +10,15 @@
 # define NULL	0
 
 void		*ft_memset(void *b, int c, size_t len);
+void		*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
 void		*memset4(u32 *restrict dst, u32 patern, size_t n);
 void		bzero(void *s, size_t n);
-void		*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void		*memccpy(void *restrict dest,
 		const void *restrict src, int c, size_t n);
-void		*ft_memmove(void *dst, const void *src, size_t len);
 void		*memchr(const void *s, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t		strlen(const char *s);
 char		*strcpy(char *dst, const char *src);
